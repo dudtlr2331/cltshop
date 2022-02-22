eac7bcb5fb09b7e307c7d10be3488f578de7c50c
whiterabbit09 <79956778+whiterabbit09@users.noreply.github.com>
2022-02-22 11:21:38 +0900

    상품 상세
    상품 상세 페이지 완성

--- WebContent/js/common.js
+++ WebContent/js/common.js
@@ -31,3 +31,38 @@ function pageMove(pageName){
         location.href = '/html/user/login/login.html';
     }
 }
+
+//상품 중간 메뉴 선택시 해당 부분으로 이동(상단 포함)
+const productMenu = document.querySelector(".product_menu_list");
+const productMenuUpperbar = document.querySelector(".product_menu_list_upperbar");
+productMenu.addEventListener("click", (event) => { //클릭 이벤트 발생시
+    const target = event.target; //마우스 클릭 타겟
+    const link = target.dataset.link; //data-link와 연동
+    if (link == null) { //data-link가 null일 경우
+        return; //아무것도 안함
+    }
+    const scrollTo = document.querySelector(link);
+    scrollTo.scrollIntoView({behavior : 'smooth'}); //이동
+});
+productMenuUpperbar.addEventListener("click", (event) => { //상단
+    const target = event.target;
+    const link = target.dataset.link;
+    if (link == null) {
+        return;
+    }
+    const scrollTo = document.querySelector(link);
+    scrollTo.scrollIntoView({behavior : 'smooth'}); //이동
+});
+
+// 일정 아래로 스크롤 하면 상품 중간 메뉴 상단에 나타남
+const productUpperbar = document.querySelector('#product_menu_upperbar');
+const productMenuTop = window.pageYOffset + productMenu.getBoundingClientRect().top - 10;
+document.addEventListener("scroll", () => {
+    if(window.scrollY > productMenuTop) {
+        productUpperbar.classList.remove('invisible');
+        productUpperbar.classList.add('visible');
+    } else {
+        productUpperbar.classList.remove('visible');
+        productUpperbar.classList.add('invisible');
+    }
+});
