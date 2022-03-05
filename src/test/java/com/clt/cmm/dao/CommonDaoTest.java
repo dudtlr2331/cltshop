e7f30ab9a50ee91e3fa751b76d6181f968b15dbe
Cat Hanbit <dudtlr102@naver.com>
2022-03-05 18:06:03 +0900

    insert, select, list 테스트

--- src/test/java/com/clt/cmm/dao/CommonDaoTest.java
+++ src/test/java/com/clt/cmm/dao/CommonDaoTest.java
@@ -5,23 +5,43 @@ import java.util.List;
 import com.clt.shp.goods.GoodsVO;
 import com.clt.shp.goods.dao.impl.GoodsDaoOracle;
 import com.clt.shp.user.UserVo;
+import com.clt.shp.user.dao.UserDao;
 import com.clt.shp.user.dao.impl.UserDaoOracle;
 
 public class CommonDaoTest {
 	public static void main(String[] args) {
 		CommonDaoTest test = new CommonDaoTest();
-	      test.insert_test();
+//	      test.insert_test();
+//	      test.select_obj_test();
+		test.select_list_test();
 	}
 	
 	public void insert_test() {
 		UserDaoOracle userDao = new UserDaoOracle();
 		UserVo pvo = new UserVo();
-		pvo.setMember_seq(4);
-		pvo.setMember_id("d");
-		pvo.setMember_name("d");
+		pvo.setMember_seq(6);
+		pvo.setMember_id("f");
+		pvo.setMember_name("f");
 		pvo.setMember_pwd("1234");
 		
 		userDao.insertUser(pvo);
+	}
+	
+	public void select_obj_test() {
+		UserDaoOracle userDao = new UserDaoOracle();
+		UserVo pvo = new UserVo();
+		pvo.setMember_seq(1);
+		UserVo obj = userDao.selectUser(pvo);
+		System.out.println(obj.toString());
+	}
+	
+	public void select_list_test() {
+		UserDaoOracle userDao = new UserDaoOracle();
+		UserVo pvo = new UserVo();
+		List<UserVo> list = userDao.selectListUser(pvo);
+		for(UserVo obj : list) {
+			System.out.println(obj.toString());
+		}
 	}
 	
 //	public void insert_test() {
@@ -34,19 +54,19 @@ public class CommonDaoTest {
 //		goodsDao.insertGoods(pvo);
 //	}
 	
-	public void select_obj_test() {
-		GoodsDaoOracle goodsDao = new GoodsDaoOracle();
-		GoodsVO pvo = new GoodsVO();
-		GoodsVO obj = goodsDao.selectGoods(pvo);
-		System.out.println(obj.toString());
-	}
-	
-	public void select_list_test() {
-		GoodsDaoOracle goodsDao = new GoodsDaoOracle();
-		GoodsVO pvo = new GoodsVO();
-		List<GoodsVO> list = goodsDao.selectListGoods(pvo);
-		for(GoodsVO obj : list) {
-			System.out.println(obj.toString());
-		}
-	}
+//	public void select_obj_test() {
+//		GoodsDaoOracle goodsDao = new GoodsDaoOracle();
+//		GoodsVO pvo = new GoodsVO();
+//		GoodsVO obj = goodsDao.selectGoods(pvo);
+//		System.out.println(obj.toString());
+//	}
+//	
+//	public void select_list_test() {
+//		GoodsDaoOracle goodsDao = new GoodsDaoOracle();
+//		GoodsVO pvo = new GoodsVO();
+//		List<GoodsVO> list = goodsDao.selectListGoods(pvo);
+//		for(GoodsVO obj : list) {
+//			System.out.println(obj.toString());
+//		}
+//	}
 }
