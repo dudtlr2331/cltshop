227fa69b08393bb156a3337257cb17c49827854f
Cat Hanbit <dudtlr102@naver.com>
2022-04-02 14:43:30 +0900

    잘못올린파일 삭제

--- src/src/main/java/com/clt/cmm/dao/DatabaseInfo.java
+++
@@ -1,8 +0,0 @@
-package com.clt.cmm.dao;
-
-public interface DatabaseInfo {
-	String DB_DRIVER = "oracle.jdbc.driver.OracleDriver";
-	String DB_URL = "jdbc:oracle:thin:@127.0.0.1:1521:xe";
-	String DB_USER = "hangang";
-	String DB_PASS = "victory123";
-}
