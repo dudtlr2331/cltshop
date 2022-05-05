68ad333c4dbb3e6113ecb8b114c14a5e6fd62d59
Syu-dev <kjiunh@gmail.com>
2022-05-05 14:43:24 +0900

    관리자 쿠폰 자바 수정

--- src/main/java/com/clt/adm/cpn/AdmCpnController.java
+++ src/main/java/com/clt/adm/cpn/AdmCpnController.java
@@ -6,15 +6,10 @@ import javax.servlet.http.HttpServletResponse;
 import com.clt.cmm.controller.Controller;
 import com.clt.cmm.servlet.HandlerMapping;
 import com.clt.cmm.servlet.ModelAndView;
-import com.clt.shp.goods.GoodsVO;
-import com.clt.shp.user.UserVo;
-import com.clt.shp.user.service.UserService;
 
 public class AdmCpnController implements Controller{
 	private ModelAndView modelAndView;
-	private AdmCpnService AdmService;
 	private String command = "";
-	private AdmCpnVo pvo;
 	
 	public AdmCpnController(String command) {
 		this.command = command;
@@ -23,10 +18,6 @@ public class AdmCpnController implements Controller{
 	@Override
 	public ModelAndView execute(HttpServletRequest req, HttpServletResponse res) throws Exception {
 		ModelAndView modelAndView = new ModelAndView();
-		
-		//파라미터 셋팅
-		AdmCpnVO pvo = parameterSetting(req);
-		
 		//비즈니스 처리
 		if(command.equals(HandlerMapping.ADM_COUPON)) {
 			modelAndView.setPath("/WEB-INF/jsp/adm/coupon/coupon.jsp");
@@ -37,20 +28,5 @@ public class AdmCpnController implements Controller{
 		req.setAttribute("leftMenuNav", HandlerMapping.ADM_GOODS_LIST);
 		//화면 반환
 		return modelAndView;
-	}
-
-	private AdmCpnVO parameterSetting(HttpServletRequest req) {
-		pvo = new AdmVo();
-	    // 파라미터 셋팅
-		
-	    String cou_name = req.getParameter("cou_name");
-	    String cou_discount = req.getParameter("cou_discount");
-	    String cou_time = req.getParameter("cou_time");
-	    String cou_count = req.getParameter("cou_count");
-	    
-	    pvo.setCouN(cou_name);
-	    pvo.setCouD(cou_discount);
-	    pvo.setCouT(cou_time);
-	    pvo.setCouC(cou_count);
 	}
 }
